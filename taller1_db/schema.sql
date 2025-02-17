9683ab074dff71737f1ad3b5824c9c7c20a7088c
JuanFGonzalezG <149331932+JuanFGonzalezG@users.noreply.github.com>
2025-02-17 15:25:39 +0000

    j

--- taller1_db/schema.sql
+++ taller1_db/schema.sql
@@ -9,7 +9,7 @@ DROP TABLE registro;
 CREATE TABLE programa (
     codigo INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre VARCHAR(50),
-    version VARCHAR(50)
+    versión VARCHAR(50)
     );
 CREATE TABLE almacenes (
     cif INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -29,7 +29,7 @@ CREATE TABLE fabricante (
     );
 CREATE TABLE progFab (
     id_fab INT,
-    codigo INT,
+    codigo INTEGER AUTOINCREMENT,
     PRIMARY KEY (id_fab, codigo)
     );
 CREATE TABLE cliente (
@@ -44,3 +44,92 @@ CREATE TABLE registro (
     codigo INT,
     PRIMARY KEY (cif, dni, codigo)
     );
+INSERT INTO programa VALUES 
+    ('Aplication Server', '9i'),
+    ('Database', '8i'),
+    ('Database', '9i'),
+    ('Database', '10g'),
+    ('Developer', '6i'),
+    ('Access', '97'),
+    ('Access', '2000'),
+    ('Access', 'XP'),
+    ('Windows', '98'),
+    ('Windows', 'XP Professional'),
+    ('Windows', 'XP Home Edition'),
+    ('Windows', '2003 Server'),
+    ('Norton Internet Security', '2004'),
+    ('Freddy Hardest', '-'),
+    ('Paradox', '2'),
+    ('C++ Builder', '55'),
+    ('DB/2', '20'),
+    ('OS/2', '10'),
+    ('JBuilder', 'X'),
+    ('La prisión', '10');
+INSERT INTO fabricante VALUES
+    ('Oracle', 'Estados Unidos'),
+    ('Microsoft', 'Estados Unidos'),
+    ('IBM', 'Estados Unidos'),
+    ('Dinamic', 'España'),
+    ('Borland', 'Estados Unidos'),
+    ('Symantec', 'Estados Unidos');
+INSERT INTO almacenes VALUES
+    ('El Corte Inglés', 'Sevilla'),
+    ('El Corte Inglés', 'Madrid'),
+    ('Jump', 'Valencia'),
+    ('Centro Mail', 'Sevilla'),
+    ('FNAC', 'Barcelona');
+INSERT INTO cliente VALUES
+    ('Pepe Pérez', 45),
+    ('Juan González', 45),
+    ('Maria Gómez', 33),
+    ('Javier Casado', 18),
+    ('Nuria Sánchez', 29),
+    ('Antonio Navarro', 58);
+INSERT INTO progFab VALUES 
+    (1),
+    (1),
+    (1),
+    (1),
+    (1),
+    (2),
+    (2),
+    (2),
+    (2),
+    (2),
+    (2),
+    (2),
+    (6),
+    (4),
+    (5),
+    (5),
+    (3),
+    (3),
+    (5),
+    (4);
+INSERT INTO progAlma VALUES
+    (1,1,10),
+    (1,2,11),
+    (1,6,5),
+    (1,7,3),
+    (1,10,5),
+    (1,13,7),
+    (2,1,6),
+    (2,2,6),
+    (2,6,4),
+    (2,7,7),
+    (3,10,8),
+    (3,13,5),
+    (4,14,3),
+    (4,20,6),
+    (5,15,8),
+    (5,16,2),
+    (5,17,3),
+    (5,19,6),
+    (5,8,8);
+INSERT INTO registro VALUES
+    (1,1,1, 'Internet'),
+    (1,3,4, 'Tarjeta postal'),
+    (4,2,10, 'Teléfono'),
+    (4,1,10, 'Tarjeta postal'),
+    (5,2,12, 'Internet'),
+    (2,4,15, 'Internet');
